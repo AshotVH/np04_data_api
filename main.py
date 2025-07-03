9c70d9a9e4def865f4e1f56cdfe6a7f138c26815
Ashot Hambardzumyan <avhmail@gmail.com>
2025-07-03 15:59:14 +0400

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ allowed_origins = [
 
 CORS(app)
 
-API_ADDRESS = os.environ.get("API_ADDRESS")
+# API_ADDRESS = os.environ.get("API_ADDRESS")
 
 @app.route('/test')
 def test():
@@ -21,27 +21,27 @@ def index():
     return ("asd")
     # return(API_ADDRESS)
 
-@app.route('/np04cachedvals', methods=['GET'])
-def np04cachedvals():
-    args = request.args
-    elemName = args.get('elemname')
-    response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
-    return response.json()
+# @app.route('/np04cachedvals', methods=['GET'])
+# def np04cachedvals():
+#     args = request.args
+#     elemName = args.get('elemname')
+#     response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
+#     return response.json()
 
-@app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
-def np04histogram(start_date, end_date, elem_id):
-    response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
-    return response.json()
+# @app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
+# def np04histogram(start_date, end_date, elem_id):
+#     response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
+#     return response.json()
 
-@app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
-def np04histogram_average(start_date, end_date, elem_id):
-    response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
-    return response.json()
+# @app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
+# def np04histogram_average(start_date, end_date, elem_id):
+#     response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
+#     return response.json()
 
-@app.route('/sensorname/<elem_id>/')
-def sensorname(elem_id):
-    response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
-    return response.json()
+# @app.route('/sensorname/<elem_id>/')
+# def sensorname(elem_id):
+#     response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
+#     return response.json()
 
 
 
