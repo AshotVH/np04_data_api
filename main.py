cbc6e4f53ac3a691cd00741184f59ffb842a9146
Ashot Hambardzumyan <avhmail@gmail.com>
2025-07-04 06:56:25 +0400

    040725 06:56

--- main.py
+++ main.py
@@ -23,29 +23,29 @@ def boo():
 @app.route('/')
 def index():
     return 'asdiopiopop'
-    # return(API_ADDRESS)
 
-# @app.route('/np04cachedvals', methods=['GET'])
-# def np04cachedvals():
-#     args = request.args
-#     elemName = args.get('elemname')
-#     response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
-#     return response.json()
 
-# @app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
-# def np04histogram(start_date, end_date, elem_id):
-#     response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
-#     return response.json()
+@app.route('/np04cachedvals', methods=['GET'])
+def np04cachedvals():
+    args = request.args
+    elemName = args.get('elemname')
+    response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
+    return response.json()
 
-# @app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
-# def np04histogram_average(start_date, end_date, elem_id):
-#     response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
-#     return response.json()
+@app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
+def np04histogram(start_date, end_date, elem_id):
+    response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
+    return response.json()
 
-# @app.route('/sensorname/<elem_id>/')
-# def sensorname(elem_id):
-#     response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
-#     return response.json()
+@app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
+def np04histogram_average(start_date, end_date, elem_id):
+    response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
+    return response.json()
+
+@app.route('/sensorname/<elem_id>/')
+def sensorname(elem_id):
+    response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
+    return response.json()
 
 
 
