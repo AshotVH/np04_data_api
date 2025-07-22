36e3715be39fd8048ad270165c7614b99eff0063
Ashot Hambardzumyan <avhmail@gmail.com>
2025-07-22 11:46:25 +0400

    back to simple jsnofiy

--- main.py
+++ main.py
@@ -9,43 +9,50 @@ app = Flask(__name__)
 
 CORS(app)
 
-# API_ADDRESS = os.environ.get("API_ADDRESS")
+API_ADDRESS = os.environ.get("API_ADDRESS")
 
 
 
+
+# @app.route('/np04cachedvals', methods=['GET'])
+# def np04cachedvals():
+#     args = request.args
+#     elemName = args.get('elemname')
+#     # response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
+#     response = requests.get("https://np04-data-api-slow-control.app.cern.ch/np04cachedvals?elemname=coldbox")
+#     raw = response.text
+#     print(raw)
+#     # Replace invalid JSON values (NaN, Infinity) with null
+#     cleaned = re.sub(r'\bNaN\b|\bInfinity\b|\b-Infinity\b', 'null', raw)
+#     try:
+#         data = json.loads(cleaned)
+#     except json.JSONDecodeError as e:
+#         return Response(f"JSON decode failed: {e}", status=500)
+
+#     # Step 4: Return valid JSON response
+#     return Response(json.dumps(data), content_type='application/json')
 
 @app.route('/np04cachedvals', methods=['GET'])
 def np04cachedvals():
     args = request.args
     elemName = args.get('elemname')
-    # response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
-    response = requests.get("https://np04-data-api-slow-control.app.cern.ch/np04cachedvals?elemname=coldbox")
-    raw = response.text
-    # Replace invalid JSON values (NaN, Infinity) with null
-    cleaned = re.sub(r'\bNaN\b|\bInfinity\b|\b-Infinity\b', 'null', raw)
-    try:
-        data = json.loads(cleaned)
-    except json.JSONDecodeError as e:
-        return Response(f"JSON decode failed: {e}", status=500)
+    response = requests.get(f"{API_ADDRESS}/latest/{elemName}")
+    return jsonify(response.json())
 
-    # Step 4: Return valid JSON response
-    return Response(json.dumps(data), content_type='application/json')
+@app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
+def np04histogram(start_date, end_date, elem_id):
+    response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
+    return jsonify(response.json())
 
+@app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
+def np04histogram_average(start_date, end_date, elem_id):
+    response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
+    return jsonify(response.json())
 
-# @app.route('/np04histogram/<elem_id>/<start_date>/<end_date>')
-# def np04histogram(start_date, end_date, elem_id):
-#     response = requests.get(f"{API_ADDRESS}/range/{start_date}/{end_date}/{elem_id}")
-#     return jsonify(response.json())
-
-# @app.route('/np04histogram_average/<elem_id>/<start_date>/<end_date>')
-# def np04histogram_average(start_date, end_date, elem_id):
-#     response  = requests.get(f"{API_ADDRESS}/average/{start_date}/{end_date}/{elem_id}")
-#     return jsonify(response.json())
-
-# @app.route('/sensorname/<elem_id>/')
-# def sensorname(elem_id):
-#     response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
-#     return jsonify(response.json())
+@app.route('/sensorname/<elem_id>/')
+def sensorname(elem_id):
+    response = requests.get(f"{API_ADDRESS}/sensor-name/{elem_id}")
+    return jsonify(response.json())
 
 
 
